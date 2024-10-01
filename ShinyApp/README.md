608db549527989c5ab3509892797fc96e5f6f896
nickhir <nick.hirschmueller@yahoo.de>
2024-10-01 11:17:57 +0200

    added ShinyApp

---
+++ ShinyApp/README.md
@@ -0,0 +1,14 @@
+# Data Explorer for Redhai & Hirschmüller _et al._, 2024
+
+This repository contains the code for the [Shiny App](https://shiny-portal.embl.de/shinyapps/app/16_IntestiMap) accompanying the publication [Redhai & Hirschmüller _et al._](https://www.biorxiv.org/), **Intestinal stem cell proliferation and differentiation depends on the self-repressive zinc finger transcription factor BCL11/Cph** (2024).
+
+The App can be accessed using this [URL](https://shiny-portal.embl.de/shinyapps/app/16_IntestiMap).
+
+## Abstract
+The molecular programs that drive proliferation and differentiation of intestinal stem cells (ISCs) are essential for organismal fitness. Notch signalling regulates the binary fate decision of ISCs, favouring enterocyte commitment when Notch activity is high and enteroendocrine cell (EE) fate when activity is low. However, the temporal dynamics of transcription factor expression and its involvement in generating distinct EE lineages across different intestinal regions remain largely uncharacterised. Here, we find that the expression of the C2H2-type zinc-finger transcription factor Chronophage (Cph), homologous to mammalian BCL11, increases early along the ISC-to-EE lineage when Notch is inactivated. We show that the expression of Cph is regulated by the Achaete-Scute Complex (AS-C) gene, scute, which directly binds to multiple sites within the Cph locus to promote its expression. Our genetic and single-cell RNA sequencing experiments demonstrate that Cph maintains the ISC and EE populations across different regions and is required to remodel the transcriptome of ISCs with low Notch activity. By performing in vivo chromatin profiling, we find that Cph activates the expression of key genes involved in proliferation and lineage commitment, while simultaneously repressing its own expression. This inhibitory feedback loop prevents ISCs from undergoing autophagy-dependent cell death and ensures differentiation is faithfully executed. Our findings shed light on the mechanism by which Cph sustains intestinal epithelial homeostasis and could represent a conserved strategy for balancing proliferation and differentiation in other tissues and species.
+
+
+## Contact
+Should you encounter any issues or have any questions please contact [Nick Hirschmüller](mailto:hirschmueller.nick@gmail.com) or [Siamak Redhai](mailto:siamak.redhai@dkfz-heidelberg.de).
+
+Raw sequencing read data along with count matrices and metadata for each sample were deposited at GEO under the Study ID: [GSE276185](https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE276185).
---
+++ ShinyApp/filtering_steps.R
@@ -0,0 +1,127 @@
+# here, we created a very reduced seurat object to save space and decrease load time
+library(here)
+library(Seurat)
+library(data.table)
+library(dplyr)
+library(Matrix)
+###################
+# For the NotchKO #
+###################
+seurat <- readRDS(here("data", "ctrl_notch_integrated_scent.rds"))
+
+
+# update the celltypes
+seurat@meta.data$celltype_manual <- ifelse(
+    seurat$high_res_annotation == "EEP",
+    "EEP",
+    seurat$celltype_manual
+)
+seurat$celltype_manual <- ifelse(seurat$celltype_manual == "unk2", "unk", seurat$celltype_manual)
+seurat$celltype_manual <- factor(seurat$celltype_manual,
+    levels = c("ISC", "EB", "EEP", "dEC", "daEC", "aEC", "mEC", "Copper", "LFC", "pEC", "EE", "MT", "unk")
+)
+
+# remove unnecessary data.
+reductions <- c("pca", "phate", "umap")
+dim_data <- data.frame(CellID = rownames(seurat@meta.data))
+
+for (reduction in reductions) {
+    dim_data <- cbind(dim_data, Embeddings(seurat, reduction)[, 1:2])
+}
+dim_data <- dim_data %>% select(-CellID)
+
+seurat_small <- DietSeurat(seurat,
+    layers = "data",
+    assays = "RNA",
+    misc = FALSE
+)
+
+
+# remove all genes that are expressed in less than 15 cells
+seurat_small <- seurat_small[rowSums(seurat_small@assays$RNA@data > 0) >= 15, ]
+
+
+# remove all counts, we will only work with normalized counts.
+# this saves a lot of space
+empty_matrix <- sparseMatrix(dims = c(nrow(seurat_small),ncol(seurat_small)), i={}, j={})
+empty_matrix <- as(empty_matrix, "dgCMatrix")
+dimnames(empty_matrix) <- dimnames(seurat_small)
+seurat_small <- SetAssayData(seurat_small, slot = "counts", new.data = empty_matrix)
+
+# only meta data that is relevant
+seurat_small@meta.data <- seurat_small@meta.data[, c("celltype_manual", "perturbation")]
+
+all(Cells(seurat_small) == rownames(dim_data))
+
+# add the dimension data to the meta data
+seurat_small@meta.data <- cbind(seurat_small@meta.data, dim_data)
+
+# change the gene name of cph
+rownames(seurat_small@assays$RNA@data) <-
+    ifelse(rownames(seurat_small@assays$RNA@data) == "CG9650", "Cph", rownames(seurat_small@assays$RNA@data))
+rownames(seurat_small@assays$RNA@counts) <-
+    ifelse(rownames(seurat_small@assays$RNA@counts) == "CG9650", "Cph", rownames(seurat_small@assays$RNA@counts))
+
+saveRDS(seurat_small, here("data", "filtered_seurat_object.rds"))
+
+############################
+# For the RNAi Experiments #
+############################
+seurat <- readRDS(here("data", "RNAi_experiments_integrated.rds"))
+# remove the unkonw cells
+seurat <- seurat[, seurat$celltype_manual != "unk"]
+seurat@meta.data$celltype_manual <- case_when(
+    seurat$high_res_annotation == "EEP" ~ "EEP",
+    T ~ seurat$celltype_manual
+)
+
+
+seurat$celltype_manual <- factor(seurat$celltype_manual,
+    levels =
+        c("ISC", "EEP", "EB", "dEC", "daEC", "aEC", "mEC", "Copper", "LFC", "pEC", "EE", "MT")
+)
+
+# remove unnecessary data.
+reductions <- c("pca", "phate", "umap")
+dim_data <- data.frame(CellID = rownames(seurat@meta.data))
+
+for (reduction in reductions) {
+    dim_data <- cbind(dim_data, Embeddings(seurat, reduction)[, 1:2])
+}
+dim_data <- dim_data %>% select(-CellID)
+DefaultAssay(seurat) <- "RNA"
+seurat_small <- DietSeurat(seurat,
+    layers = "data",
+    assays = "RNA",
+    misc = FALSE
+)
+
+# remove all genes that are expressed in less than 15 cells
+seurat_small <- seurat_small[rowSums(seurat_small@assays$RNA@data > 0) >= 15, ]
+
+# remove all counts, we will only work with normalized counts.
+# this saves a lot of space
+empty_matrix <- sparseMatrix(dims = c(nrow(seurat_small),ncol(seurat_small)), i={}, j={})
+empty_matrix <- as(empty_matrix, "dgCMatrix")
+dimnames(empty_matrix) <- dimnames(seurat_small)
+seurat_small <- SetAssayData(seurat_small, slot = "counts", new.data = empty_matrix)
+
+
+# only meta data that is relevant
+seurat_small@meta.data <- seurat_small@meta.data[, c("celltype_manual", "perturbation")]
+
+all(Cells(seurat_small) == rownames(dim_data))
+
+# add the dimension data to the meta data
+seurat_small@meta.data <- cbind(seurat_small@meta.data, dim_data)
+
+seurat_small$perturbation <- factor(seurat_small$perturbation, levels = c("ctrl", "NotchRNAi", "NotchCphRNAi", "CphUp"))
+
+# change the gene name of cph
+rownames(seurat_small@assays$RNA@data) <-
+    ifelse(rownames(seurat_small@assays$RNA@data) == "CG9650", "Cph", rownames(seurat_small@assays$RNA@data))
+rownames(seurat_small@assays$RNA@counts) <-
+    ifelse(rownames(seurat_small@assays$RNA@counts) == "CG9650", "Cph", rownames(seurat_small@assays$RNA@counts))
+
+saveRDS(seurat_small, here("data", "filtered_seurat_object_RNAi.rds"))
+
